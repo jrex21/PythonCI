25c9ba41b4103a77f685a9b36fed21ea68088832
jrex21 <jrex@g.clemson.edu>
2014-02-24 12:06:13 -0500

    ifthenloopdemo
    demo for learning activity

---
+++ ifthenloopdemo.py
@@ -0,0 +1,10 @@
+# Jessica Rex
+# Python code for exploratory activity on if-then loops
+
+moms_answer = input("Mom, can I have cake? ")
+
+if moms_answer == "yes":
+    print("\nYou may have some cake!")
+    
+if moms_answer == "no":
+    print("\nSorry, you can't have any cake right now.")
